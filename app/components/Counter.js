80b5448aaa85ebdaa87c85d1ac654d209aa264e3
noprom <tyee.noprom@qq.com>
2016-11-26 22:15:33 +0800

    add Counter

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -1,1 +1,22 @@
 'use strict';
+
+import React, {Component, PropTypes} from 'react';
+import {Text, View, Button} from 'react-native';
+
+export default class Counter extends Component {
+  static propTypes = {
+    onPress: PropTypes.object
+  };
+
+  render() {
+    const {text, onPress} = this.props;
+    return (
+      <View>
+        <Text style={{fontSize: 15, alignSelf: 'center'}}>{text}</Text>
+        <Button title="add" onPress={onPress.increment} />
+        <Text>Hello World!</Text>
+        <Button title="subtract" onPress={onPress.decrement} />
+      </View>
+    );
+  }
+}
