dde253dd105538114b252a0aabc0704dadaa734d
noprom <tyee.noprom@qq.com>
2016-11-26 22:22:53 +0800

    init main page

--- app/containers/MainPage.js
+++ app/containers/MainPage.js
@@ -1,1 +1,19 @@
 'use strict';
+
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as Actions from '../actions/actionCreator';
+import Counter from '../components/Counter';
+
+class MainPage extends Component {
+  render(
+    const {number, actions} = this.props;
+    return (
+      <View>
+        <Counter text={number} onPress={actions} />
+      </View>
+    );
+  );
+}
