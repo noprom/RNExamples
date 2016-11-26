958a81bd3bbfdf0e40a0078080a82425faf758ce
noprom <tyee.noprom@qq.com>
2016-11-26 23:10:07 +0800

    add app

---
+++ app/App.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import {store} from './store/index';
+import {Provider} from 'react-redux';
+import React from 'react';
+import {Navigator} from 'react-native';
+import MainPage from './containers/MainPage';
+
+export default class App extends React.Component {
+  render (
+    return (
+      <Provider store = {store}>
+        <Navigator
+          initialRoute = {{component: MainPage}},
+          renderScene = {(route, navigator) => {
+            return <route.component navigator={navigator} {...route.args}/>
+          }}
+        />
+      </Provider>
+    );
+  );
+}
